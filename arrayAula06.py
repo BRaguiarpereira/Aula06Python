34c4e0ecc04bfeb1038e3562b9a6ff4b9b8b1940
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-10-08 19:59:47 -0300

    aps
    tudo sobre a aps

---
+++ arrayAula06.py
@@ -0,0 +1,88 @@
+'''
+Array (VetoresEmatrizes)
+Um vetor(array) é um conjunto ordenado de variaveis de um mesmo tipo.
+
+Definimos:
+
+-tipo do dado
+-nome
+-tamanho(da celula)
+
+obs: cada celula e numerada,começando em 0 indo até tamanho-1
+
+Vetores sao normalmente usados com comandos de repetiçao (for ou while)
+
+No Python , nao há vetores,e sim listas.Porem o tipo string se comporta como vetor
+
+EXEMPLO:
+
+'''
+
+'''
+texto = 'Python'
+for i in range(0,6):
+    print(texto[i])
+'''
+    
+'''
+len(string1): devolve o tamanho de um string ou uma lista
+'''
+
+'''
+teste = input('Digite algo :')
+print('{} caracteres'.format(len(teste)))
+'''
+
+'''
+Operaçoes com strings
++: concotena 2 strings
+teste1 = 'CC'
+teste2 = 'SI'
+print(teste1+teste2)
+print(teste2+teste1)
+
+N*str1{
+
+str1+str1+str1+str1+str1+str1
+
+}
+'''
+
+'''
+teste1='teste'
+teste2='python'
+print(teste1+teste2)
+print(2*(teste1+teste2))
+print('primeira string : {} caracteres'.format(len(teste1)))
+print('segunda string : {} caracteres'.format(len(teste2)))
+'''
+
+'''
+Outros metodos para manipular strings
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
